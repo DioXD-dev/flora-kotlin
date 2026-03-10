0b002672ad14f07b555c978bb690b19e430228bc
BerakGG <mcpeid500@gmail.com>
2026-03-10 16:03:19 +0700

    fix: add jitpack repository for Glide

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
-    }
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
 }
 rootProject.name = "FloraMusic"
 include(":app")
